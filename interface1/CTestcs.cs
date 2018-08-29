9940d1aa92e2ec008cd5855dd0522528ea98c627
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-08-29 13:09:43 +0800

    完善CTest 判斷成績

--- interface1/CTestcs.cs
+++ interface1/CTestcs.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace interface1
 {
-    class CTestcs
+    class CTestcs : Ipass
     {
+        private int score = 60;
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        private string passOK;
+
+        public string PassOrNot(int grade)
+        {
+            if (grade >= score)
+                passOK = "you are pass~ Well Done!";
+            else
+                passOK = "you are Fail!!!";
+            return passOK;
+        }
+
+
     }
 }
