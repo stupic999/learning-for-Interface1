39f6fe16e39070f47c179ba7e43c1f18feb0bffd
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-08-29 13:14:56 +0800

    CTest繼承所有界面 並可判斷成績優異或差

--- interface1/CTestcs.cs
+++ interface1/CTestcs.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace interface1
 {
-    class CTestcs : Ipass
+    class CTestcs : Ipass,IGoodOrBad
     {
         private int score = 60;
         public int Score
@@ -26,6 +26,35 @@ namespace interface1
             return passOK;
         }
 
+        private string goodOrBad;
+        private int high = 80;
+        public int High
+        {
+            get { return high; }
+            set { high = value; }
+        }
+        private int low = 50;
+        public int Low
+        {
+            get { return low; }
+            set { low = value; }
+        }
 
+        public string GoodOrBad(int grade)
+        {
+            if (grade >= High)
+            {
+                goodOrBad = "You are very good!";
+            }
+            else if (grade < Low)
+            {
+                goodOrBad = "You are so lousy...";
+            }
+            else
+            {
+                goodOrBad = "You are nice.";
+            }
+            return goodOrBad;
+        }
     }
 }
